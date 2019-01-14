554b1b5e440d959769f191442e4cf96f02c461a6
Mluocheng <873242241@qq.com>
2019-01-14 16:19:22 +0800

    修补bug

---
+++ demo.php
@@ -0,0 +1,20 @@
+<?php
+
+include_once "wxBizDataCrypt.php";
+
+
+$appid = 'wx1e4785fa849e936c';
+$sessionKey = 'OaGZH0YKQlLI5bxs5H64tw==';
+
+$encryptedData="eW0IE28B+aI+e2ScchCKUVS3mCBzNtnUKgFfgH/sGvzIVXCWEWW//TAlND/bPG4hgtY+d0wButHz6QqgN0aM6o0srJVFQkS5FNKDy/Pby+rDx0457V0ESDtykuzHWVQVoj7TrfyXmZibl289f3cVlzGaBM9dwQeRwYbyolo4nQS/9luzogOsGnMSQDQHoJSS01z24qfTXIiHQIrSs3s8dw==";
+
+$iv = 'NsocrwB5E2ng5jCKBPHOZg==';
+
+$pc = new WXBizDataCrypt($appid, $sessionKey);
+$errCode = $pc->decryptData($encryptedData, $iv, $data );
+
+if ($errCode == 0) {
+    print($data . "\n");
+} else {
+    print($errCode . "\n");
+}
